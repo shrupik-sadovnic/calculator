6f2667990b0b5c6b1b2c15a96c35b5491b06f649
shrupik-sadovnic <shrupik-sadovnic@mail.ru>
2026-02-16 12:33:12 +0600

    Исходный коммит. Создана базовая структура проекта и точка входа - класс App.

---
+++ src/ru/oivt/rectanglecli/App.java
@@ -0,0 +1,14 @@
+package ru.oivt.rectanglecli;
+
+public class App {
+
+	public App() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
