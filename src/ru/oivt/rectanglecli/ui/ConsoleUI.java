50f1937f07b28cc48dc0957a37953d7efb1f23d8
shrupik-sadovnic <shrupik-sadovnic@mail.ru>
2026-03-15 01:38:28 +0600

    Добавлено меню с расчетом периметра/площади и выходом

--- src/ru/oivt/rectanglecli/ui/ConsoleUI.java
+++ src/ru/oivt/rectanglecli/ui/ConsoleUI.java
@@ -1,6 +1,7 @@
 package ru.oivt.rectanglecli.ui;
 
 import ru.oivt.rectanglecli.util.InputUtils;
+import ru.oivt.rectanglecli.model.Rectangle;
 import java.util.Scanner;
 
 public class ConsoleUI {
@@ -13,17 +14,36 @@ public class ConsoleUI {
         double a = InputUtils.readPositiveDouble(scanner, "Введите сторону a: ");
         double b = InputUtils.readPositiveDouble(scanner, "Введите сторону b: ");
 
+        Rectangle rectangle = new Rectangle(a, b);
         System.out.println("Создан прямоугольник со сторонами " + a + " и " + b);
-        showMenu();
+        showMenu(rectangle);
     }
 
     private void printHeader() {
         System.out.println("=== Программа расчета прямоугольника ===");
     }
 
-    private void showMenu() {
-        System.out.println("Меню (заглушка):");
-        System.out.println("1. Опции пока недоступны");
-        System.out.println("0. Выход");
+    private void showMenu(Rectangle rect) {
+        while (true) {
+            System.out.println("\nМеню:");
+            System.out.println("1. Вычислить периметр");
+            System.out.println("2. Вычислить площадь");
+            System.out.println("0. Выход");
+            int choice = InputUtils.readInt(scanner, "Ваш выбор: "); // вызов с подсказкой
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Периметр: " + rect.perimeter());
+                    break;
+                case 2:
+                    System.out.println("Площадь: " + rect.area());
+                    break;
+                case 0:
+                    System.out.println("Выход...");
+                    return;
+                default:
+                    System.out.println("Некорректный выбор, попробуйте снова.");
+            }
+        }
     }
 }
